5efa7eec38877d1db1f540a1ab5cead6a3a7ce25
yash <singhyashkun@gmail.com>
2024-05-13 14:48:07 +0530

    Second Commit

---
+++ dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // Treats all code as a newer version
+
+// alert(3+3) // we are using nodejs, not browser. This gives an alert popup uin browser with message 6.
+
+console.log(3+3)
+
+console.log("Yash")
+
+
+// String data type
+let name = "Yash"
+
+// Number data type.
+let age = 24
+
+// Boolean data type
+let isAdult = true
+
+// null data type
+// undefined data type -> no value has been given.
+// symbol -> for uniquness.
+
+// Object data type.
+
+console.log(typeof "Yash") //string
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
--- text.js
+++ text.js
@@ -1,1 +1,32 @@
-console.log("Yash")
+// Defines a constant.
+const accountId = 2000080635
+
+// Defines a variable in modern syntax. Proper way.
+let accountEmail = "singhyashkun@gmail.com"
+
+// Prefer not to use because of issue in block and functional scope.
+var accountPassword = "123456789"
+
+// Just declaring a variable 
+accountCity = "Prayagraj"
+
+// declaring a variable with no value. Value will be undefined.
+let accountState
+
+// Cannot change value of a constant.
+// accountId = 2000080636
+
+accountPassword = "987654321"
+accountEmail = "yash.vardhan566@gmail.com"
+accountCity = "Allahabad"
+
+
+
+console.log(accountId)
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
+
+accountState = "UP"
+
+// console.log(accountState)
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
