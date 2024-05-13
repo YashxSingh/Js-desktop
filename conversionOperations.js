c537b94adb90e6dba6e7f00ddac2b3ba6ab3af48
yash <singhyashkun@gmail.com>
2024-05-13 15:55:42 +0530

    V3

---
+++ conversionOperations.js
@@ -0,0 +1,43 @@
+let score = null
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let score_2 = Number(score)
+// console.log(typeof score_2)
+// console.log(score_2)
+
+// "33" -> 33
+// "33aa" -> NaN
+// true -> 1, false -> 0
+// undefined -> NaN
+// null -> 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let nameNumber = 44
+let stringConv = String(nameNumber)
+// console.log(stringConv)
+// console.log(typeof stringConv)
+
+
+// ****************** Operations ***************************8
+let value = -89
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "Yash"
+let str2 = "How are you?"
+
+let str3 = str1 + str2
+console.log(str3)
+console.log(str1, str2)
