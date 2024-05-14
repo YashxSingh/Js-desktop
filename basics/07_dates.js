487734ef239dc6486b80da4e903c2d85204cf244
yash <singhyashkun@gmail.com>
2024-05-14 21:04:02 +0530

    14.05

---
+++ basics/07_dates.js
@@ -0,0 +1,25 @@
+const myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString());
+
+// Custom dates.
+// let myCustomDate = new Date(2024, 6, 8, 5, 59, 48, 88);
+// let myCustomDate = new Date("2024-5-8");
+let myCustomDate = new Date("01-11-1999");
+// console.log(myCustomDate.toLocaleString());
+// console.log(myCustomDate.toLocaleTimeString());
+
+let myTimestamp = Date.now()
+
+// console.log(myTimestamp);
+// console.log(Math.floor(Date.now()/1000));
+
+console.log(myDate.toLocaleString('default', {
+    // era: "long",
+    weekday: "short",
+    year:"numeric"
+}));
